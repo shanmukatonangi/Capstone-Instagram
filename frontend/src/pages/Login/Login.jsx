43a69d4ef7fc99ab377f38884f2225e75f54cf8a
shanmukatonangi <shanmukatonangi@gmail.com>
2024-09-12 19:59:50 +0530

    sept 12 login register

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -3,12 +3,47 @@ import "./login.css"
 
 const Login = () => {
   return (
-    <div className='login'>
+    <div className="login">
 
-        <div className='login-image'></div>
-        <div className='login-feilds'></div>
-        
-      
+        <div className="login-image"></div>
+
+        <div className="login-feilds">
+            <div className="input-feilds">
+            <div className="logo">
+                <img src="https://fontmeme.com/images/instagram-new-logo.png" />
+            </div>
+            <div className="details">
+                <input type="text" id="username" name="username" placeholder="Phone number , email , username" />
+                <input type="password" id="password" name="password" placeholder="Password" />
+                <button>log in</button>
+            </div>
+            <div className="forgot">
+                <div className="fg"> <img src="https://tse4.mm.bing.net/th?id=OIP.wOPYZ0FBrg5cMj5uwD00nwHaHa&pid=Api&P=0&h=180" /><p>Login with Facebook</p></div>
+                <a href="#">Forgot password?</a>
+            </div>
+
+
+            </div>
+
+            <div className="sign-up">
+                <p>Don't have an account? <a id="sup" href="/register">Sign Up</a></p>
+            </div>
+
+            <div className="apps">
+                <p>Get the apps</p>
+                <div className="app-stores">
+                    <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D25A9ECA6-51ED-418E-9167-8F0BD7FDA9CD%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge">
+                        <img src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png" className="logos" />
+                    </a>
+                    <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D25A9ECA6-51ED-418E-9167-8F0BD7FDA9CD%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge">
+                        <img src="https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png" className="logos"/>
+                    </a>
+                </div>
+            </div>
+
+
+        </div>
+
     </div>
   )
 }
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -3,9 +3,54 @@ import "./register.css"
 
 const Register = () => {
   return (
-    <div>
-      R
+    <div class="register">
+
+    <div class="register-image"></div>
+
+    <div class="register-feilds">
+        <div class="input-feilds">
+        <div class="logo">
+            <img src="https://fontmeme.com/images/instagram-new-logo.png" />
+        </div>
+
+        <div class="forgot">
+            <div class="fg"> Sign up to see photos and videos from your friends.</div>
+           
+        </div>
+
+
+        <div class="details">
+            <input placeholder="Mobile Number" />
+            <input placeholder="Full name" />
+            <input type="text" id="username" name="username" placeholder="Username" />
+            <input type="password" id="password" name="password" placeholder="Password" />
+            <button>Sign up</button>
+        </div>
+       
+
+
+        </div>
+
+        <div class="sign-up">
+            <p>Have an account? <a id="sup" href="/login">Log in</a></p>
+        </div>
+
+        <div class="apps">
+            <p>Get the apps</p>
+            <div class="app-stores">
+                <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D25A9ECA6-51ED-418E-9167-8F0BD7FDA9CD%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge">
+                    <img src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png" class="logos" />
+                </a>
+                <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D25A9ECA6-51ED-418E-9167-8F0BD7FDA9CD%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge">
+                    <img src="https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png" class="logos"/>
+                </a>
+            </div>
+        </div>
+
+
     </div>
+
+</div>
   )
 }
 
