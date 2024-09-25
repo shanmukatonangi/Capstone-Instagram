0af3f2a15af086643ee5021cad3b3d389ef380dc
shanmukatonangi <shanmukatonangi@gmail.com>
2024-09-25 21:30:06 +0530

    sept25

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,8 +4,34 @@ import SideBar from '../../components/SideBar'
 
 const Profile = () => {
   return (
-    <div>
+    <div className="profile-box">
       <SideBar />
+      <div className="profile">
+        <div className='profile-things'>
+          <div className='profile-container'>
+            <div className='profile-picture'>
+              <input type='file'  className='pp' />
+            </div>
+            <div className='profile-info'>
+              <div classname="username-box" id='ub'>
+                <p>Shanmukh.t</p>
+                <button className='setting'>Edit Profile</button>
+                <button className="setting">View Archeive</button>
+              </div>
+
+              <div className='user-p'>
+                <p>0 posts</p>
+                <p>0 following</p>
+                <p>0 followers</p>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
     
     </div>
   )
