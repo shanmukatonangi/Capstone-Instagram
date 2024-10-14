560502c27a0c97942f1b0d0a3eb8505525585126
shanmukatonangi <shanmukatonangi@gmail.com>
2024-10-14 21:28:03 +0530

    oct14

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://b22:b22@cluster0.nrwpf.mongodb.net/',console.log("DB connected"));
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 
 app.listen(8888,()=>{
